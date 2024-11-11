c6eb8fe191ca2e5ac347380f889cee70fa4349dd
Swarna Dutta <swarnadutta0017@gmail.com>
2024-11-11 20:24:42 +0530

    Atmosphere class updated with density calculating function

--- AirData.py
+++ AirData.py
@@ -2,7 +2,28 @@
 
 import numpy as np
 import math
-from U_inputs import *
+from U_inputs import U_Inputs_Simulator
+
+# This class gives you the density, pressure, speed of sound at a particular altitude
 class Atmosphere():
-    def __init__(self):
-        self.alt=altitude
+    rho_0       = 1.225                                     # Standard air density MSL, kg/m^3
+    T_0         = 298                                       # Standard temperature MSL, K
+    P_0         = 101325                                    # Standard Pressure at MSL, Pa
+    Temp_grad   = -6.5*10**(-3)                             # Slope upto 11 km considering max service ceiling of a helicopter
+    def __init__(self, simulator_inputs: U_Inputs_Simulator, rho_0:float, T_0:float, P_0: float, Temp_grad: float):
+        self.alt=simulator_inputs.Altitude
+        self.rho_0   = rho_0                                     
+        self.Temp_0  = T_0 
+        self.P_0     = P_0 
+        self.Temp_grad = Temp_grad
+        self.T1 = self.T_0+self.Temp_grad*self.alt
+
+    def rho_calc(self):
+        rho=self.rho_0*((self.T1/self.T_0)**4.2586)
+        return rho
+    
+    def speed_of_sound_calc(self):
+        speed_of_sound = math.sqrt(1.4*287*self.T1)         # Specific gas constant for air, R = 287 kJ/kg-K, Gamma=1.4 for air
+        return speed_of_sound
+
+
