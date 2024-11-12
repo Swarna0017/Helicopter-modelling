a23eb7f787d3f430894f4496da53ae102b42ce4a
Swarna Dutta <swarnadutta0017@gmail.com>
2024-11-12 11:55:29 +0530

    added the mission planner file

---
+++ Mission_Planner.py
@@ -0,0 +1,17 @@
+# This file contains the code for mission planning of hover, climb and forward flight segments
+import Flight_Simulator
+from U_inputs import U_Inputs_Planner
+from Blade_G import *
+from AirData import Atmosphere
+
+class M_Planner_Hover():
+    def __init__(self) -> None:
+        pass
+
+class M_Planner_Forward_Flight():
+    def __init__(self) -> None:
+        pass
+
+class M_Planner_Climb():
+    def __init__(self) -> None:
+        pass
