e784fed0eb66f4543e346a145fe255a2d1fa3dc4
Swarna Dutta <swarnadutta0017@gmail.com>
2024-11-12 16:41:45 +0530

    sim_data class added in integrator

--- Instantaneous_Integrator.py
+++ Instantaneous_Integrator.py
@@ -2,6 +2,7 @@
 from U_inputs import U_Inputs_Simulator                         # Specified input files are called from here
 from AirData import Atmosphere
 from Blade_G import Blade                                       # For importing the relevant blade parameters like chord length, taper, etc.
+import numpy as np
 
 # I have added the classes for implementing different hover performance prediction methods here
 class MT_Implementor():
@@ -50,3 +51,61 @@ class Thrust():
         Ct=Thrust_hover/(self.rho*self.A*(self.omega*self.MRR)**2)
         Cp=Power/(self.rho*self.A*(self.omega*self.MRR)**3)
         return Ct, Cp
+    
+class simulation_data():
+    # This class brings together all the force functions written above.
+    def __init__(self):
+        pass
+
+    # Functions to generate forces data
+    def generate_forces_x(self, collective_pitch, lateral_pitch, longitudinal_pitch, tail_rotor_collective):
+        x = np.linspace(0, 10, 100)
+        y = np.sin(x + collective_pitch) + lateral_pitch + 0.5 * longitudinal_pitch
+        return x, y
+
+    def generate_forces_y(self, collective_pitch, lateral_pitch, longitudinal_pitch, tail_rotor_collective):
+        x = np.linspace(0, 10, 100)
+        y = np.cos(x + tail_rotor_collective) + longitudinal_pitch - 0.3 * lateral_pitch
+        return x, y
+
+    def generate_forces_z(self, collective_pitch, lateral_pitch, longitudinal_pitch, tail_rotor_collective):
+        x = np.linspace(0, 10, 100)
+        y = np.sin(x) * np.cos(tail_rotor_collective + lateral_pitch) + 0.2 * longitudinal_pitch
+        return x, y
+
+    def generate_forces_xyz(self, collective_pitch, lateral_pitch, longitudinal_pitch, tail_rotor_collective):
+        x = np.linspace(0, 10, 100)
+        # Separate the components
+        force_x = np.sin(x + collective_pitch)
+        force_y = np.cos(x + tail_rotor_collective)
+        force_z = np.sin(x) * np.cos(tail_rotor_collective + lateral_pitch) + 0.2 * longitudinal_pitch
+
+        # Combine the individual components
+        return x, force_x, force_y, force_z
+
+    # Functions to generate moments data
+    def generate_moments_x(self, collective_pitch, lateral_pitch, longitudinal_pitch, tail_rotor_collective):
+        x = np.linspace(0, 10, 100)
+        y = np.cos(x + collective_pitch) + np.sin(
+            x) + 0.4 * lateral_pitch - 0.2 * longitudinal_pitch + tail_rotor_collective
+        return x, y
+
+    def generate_moments_y(self, collective_pitch, lateral_pitch, longitudinal_pitch, tail_rotor_collective):
+        x = np.linspace(0, 10, 100)
+        y = np.sin(x) + collective_pitch - 0.7 * lateral_pitch + 0.5 * longitudinal_pitch
+        return x, y
+
+    def generate_moments_z(self, collective_pitch, lateral_pitch, longitudinal_pitch, tail_rotor_collective):
+        x = np.linspace(0, 10, 100)
+        y = np.cos(x + longitudinal_pitch) + collective_pitch - 0.6 * tail_rotor_collective + lateral_pitch
+        return x, y
+
+    def generate_moments_xyz(self, collective_pitch, lateral_pitch, longitudinal_pitch, tail_rotor_collective):
+        x = np.linspace(0, 10, 100)
+        # Separate the components
+        moment_x = np.cos(x + collective_pitch) + np.sin(x)
+        moment_y = np.sin(x) + collective_pitch - 0.7 * lateral_pitch + 0.5 * longitudinal_pitch
+        moment_z = np.cos(x + longitudinal_pitch) + collective_pitch - 0.6 * tail_rotor_collective + lateral_pitch
+
+        # Combine the individual components
+        return x, moment_x, moment_y, moment_z
