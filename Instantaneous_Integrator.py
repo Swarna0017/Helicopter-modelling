884cf903808b09e04e0f438a352aab1fb12ad73e
Swarna Dutta <swarnadutta0017@gmail.com>
2024-11-15 18:41:52 +0530

    debugs, formatring

--- Instantaneous_Integrator.py
+++ Instantaneous_Integrator.py
@@ -10,31 +10,31 @@ import numpy as np
 # All classes for implementing different hover performance prediction methods here
 class BEMT_Implementer():
     def __init__(self, simulator_inputs=U_Inputs_Simulator, pilot_inputs=Pilot_Inputs, Blade=Blade, Atmosphere_data= Atmosphere, Airfoil=Airfoil_data, v_data= v_calculator):
-        self.rho_0      = 1.225             #kg/m^3   
-        Atmosphere_data = Atmosphere(simulator_inputs, pilot_inputs)
-        Blade = Blade(simulator_inputs=simulator_inputs, pilot_inputs=pilot_inputs)     
-        self.simulator_inputs=simulator_inputs
-        self.pilot_inputs=Pilot_Inputs 
-        self.rho        = Atmosphere_data.rho_calc()
-        self.V          = simulator_inputs.V
-        self.MRR        = simulator_inputs.MRR
-        self.MRA        = simulator_inputs.MRA
-        self.omega      = simulator_inputs.MR_omega
-        self.MR_nb      = simulator_inputs.MR_nb
-        self.MR_rc      = simulator_inputs.MR_rc
-        self.theta      = pilot_inputs.theta_0
-        self.MR_omega   = (simulator_inputs.MR_omega)*math.pi*2/60
-        self.VW         = simulator_inputs.VW
-        self.A          = simulator_inputs.MRA
-        self.r          = Blade.Blade_sections(10)
-        self.v          = v_data.v_hover(self)  # Accessing v_hover from the instance
-        self.chord_r    = Blade.chord()
-        self.phi        = Airfoil.Phi(self)
-        self.range      = simulator_inputs.Iterations
-        self.v_data     = v_data(Blade, simulator_inputs)  # Creating an instance of v_calculator
-        self.aoa        = Airfoil.AOA(self)  
-        self.dr         = Blade.dr
-        self.MR_chord   = simulator_inputs.MR_chord
+        self.rho_0              = 1.225             # kg/m^3   
+        Atmosphere_data         = Atmosphere(simulator_inputs, pilot_inputs)
+        Blade                   = Blade(simulator_inputs=simulator_inputs, pilot_inputs=pilot_inputs)     
+        self.simulator_inputs   =simulator_inputs
+        self.pilot_inputs       =Pilot_Inputs 
+        self.rho                = Atmosphere_data.rho_calc()
+        self.V                  = simulator_inputs.V
+        self.MRR                = simulator_inputs.MRR
+        self.MRA                = simulator_inputs.MRA
+        self.omega              = simulator_inputs.MR_omega
+        self.MR_nb              = simulator_inputs.MR_nb
+        self.MR_rc              = simulator_inputs.MR_rc
+        self.theta              = pilot_inputs.theta_0
+        self.MR_omega           = (simulator_inputs.MR_omega)*math.pi*2/60
+        self.VW                 = simulator_inputs.VW
+        self.A                  = simulator_inputs.MRA
+        self.r                  = Blade.Blade_sections(10)
+        self.v                  = v_data.v_hover(self)  # Accessing v_hover from the instance
+        self.chord_r            = Blade.chord()
+        self.phi                = Airfoil.Phi(self)
+        self.range              = simulator_inputs.Iterations
+        self.v_data             = v_data(Blade, simulator_inputs)  # Creating an instance of v_calculator
+        self.aoa                = Airfoil.AOA(self)  
+        self.dr                 = Blade.dr
+        self.MR_chord           = simulator_inputs.MR_chord
 
 
         self.Thrust, self.Torque, self.Power = self.BEMT_Solver()
